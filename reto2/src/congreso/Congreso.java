04b2d023c2913544e167c7d088857d0780ed86d2
Jonathan Mauricio Meza Bastidas <53632260+JonathanMauricioMezaBastidas@users.noreply.github.com>
2021-08-22 22:37:18 -0500

    MAIN

--- reto2/src/congreso/Congreso.java
+++ reto2/src/congreso/Congreso.java
@@ -67,7 +67,7 @@ public class Congreso {
         } while (opc != 3);
 
     }
-
+    // FUNCIÓN PRINCIPAL
     public static void main(String[] args) {
         Congreso congreso1 = new Congreso("Congreso Nacional de Computacion");
         congreso1.procesarComandos();
